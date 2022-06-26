62b60f9eb764a32d999e015422672c82f10358bc
bastian rodríguez <basrodriguezr@gmail.com>
2022-06-26 01:22:05 -0400

    text area funcionales version 1

---
+++ main.js
@@ -0,0 +1,62 @@
+const $ = selector => document.querySelector(selector);
+
+const $body = $('#body');
+const $js = $('#js');
+const $css = $('#css');
+
+const magia = () => {
+    const body = $body.value;
+    const js = $js.value;
+    const css = $css.value;
+    
+    let resultado=
+        `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <style type="text/css">
+                ${css}
+            </style>
+        </head>
+        <body>
+            ${body}
+            <script type="text/javascript">
+                ${js}
+            </script>
+        </body>
+        </html>
+        `
+        
+    return resultado;
+}
+
+const update = () => {
+    const body = magia();
+    $('#magia').setAttribute('srcdoc', body);
+}
+
+$body.addEventListener('input', e => {
+    console.log('si actualizo por aqui: body');
+    update();
+});
+
+$js.addEventListener('input', e => {
+    console.log('si actualizo por aqui: js');
+    update();
+});
+
+$css.addEventListener('input', e => {
+    console.log('si actualizo por aqui: css');
+    update();
+});
+
+Split({
+    columnGutters: [{
+        track: 1,
+        element: document.querySelector('.gutter-col-1'),
+    }],
+    rowGutters: [{
+        track: 1,
+        element: document.querySelector('.gutter-row-1'),
+    }]
+})
